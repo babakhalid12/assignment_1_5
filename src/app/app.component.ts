a288fb91130ae2667ec4889468f8657bcd1f05f7
unknown <babakhalid.patel@gmail.com>
2018-06-21 23:14:25 +0530

    commiting

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div style="text-align:center">
+  <h1>
+    
+  Marvellous Infosystems</h1>
+  <br>
+  <h2>“Educating for better tomorrow"</h2>
+</div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
